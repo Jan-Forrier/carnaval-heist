d80cbe9335938af652055d55fab16a5b5d66c956
Jan Forrier <jan.forrier@knokke-heist.be>
2026-02-03 09:52:24 +0100

    aanpassen base URL

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://heistcarnaval.be'
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://heistviertcarnaval.be'
 
   return {
     rules: [
