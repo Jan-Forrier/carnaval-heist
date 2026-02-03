44a63ffd81264a4e00119a56742f2517cdd0872e
Jan Forrier <jan.forrier@knokke-heist.be>
2026-02-03 09:47:43 +0100

    aanpassen sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,13 +5,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
 
   return [
     {
-      url: baseUrl,
+      url: `${baseUrl}/`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
     },
     {
-      url: `${baseUrl}/cookieverklaring`,
+      url: `${baseUrl}/cookieverklaring/`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.5,
